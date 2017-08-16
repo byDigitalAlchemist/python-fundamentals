ffb25834b467826e38be22eb73e587e3a90a57cd
Digital Alchemist <developer.anwar@gmail.com>
2017-08-16 01:43:31 -0400

    Initial commit

---
+++ basic-one.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+A = [1,2,3]
+B = np.array([11,12,13])
+
+for numbers in B:
+    print (numbers);
+
+B = B + 1
+
+for numbers in B:
+    print (numbers);
+
+
+np.sqrt(A)
+np.sqrt(B)
+
+print (A)
+
+print (B)
